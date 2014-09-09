be5033819343c8bb8ae9c0a1fdc5dc631c1fb918
Clark Stevenson <clark@drimlike.com>
2014-09-09 23:46:25 +0100

    Testing account linkage (multiple emails)

--- p2.d.ts
+++ p2.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for p2.js v0.6.0
 // Project: https://github.com/schteppe/p2.js/
-// Definitions by: Clark Stevenson <https://github.com/clark-stevenson/>
 
 declare module p2 {
 
